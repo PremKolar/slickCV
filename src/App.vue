2e481c001ed83d99590958bf8f964f3c188e3912
Nikolaus Koopmann <nikolauskoopmann@gmail.com>
2021-04-30 12:50:26 +0200

    wip

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <SlickCV :resume="resume"></SlickCV>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import resumeData from "@/assets/resume.json";
+import SlickCV from "@/components/SlickCV.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    SlickCV,
+  },
+  setup() {
+    console.log(resumeData);
+    const resume = resumeData;
+    return { resume };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
